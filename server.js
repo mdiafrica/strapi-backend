5ebd64ae114ec288c4bb07c21eb02e2d28279f64
Miriam Uwingabiye <miriamuwingabiye@ashesi.edu.gh>
2026-06-28 00:00:46 +0200

    Add server.js entry point for Hostinger

---
+++ server.js
@@ -0,0 +1,12 @@
+const { createStrapi, compileStrapi } = require('@strapi/strapi');
+
+async function main() {
+  const appContext = await compileStrapi();
+  const app = await createStrapi(appContext).load();
+  app.start();
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
